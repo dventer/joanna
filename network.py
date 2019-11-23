220cf64faa7a2a43c87748d37d4129bdf82c9463
jefri <jefri.adv@gmail.com>
2019-11-23 18:12:15 +0700

    add new function

--- network.py
+++ network.py
@@ -1,11 +1,10 @@
 from netmiko import Netmiko
-from datetime import datetime
 from time import sleep
 import paramiko
 
 
 class Network:
-    def __init__(self, username, password=None, keys=None):
+    def __init__(self, username, password=None, keys=True):
         '''
         :param username: username of device
         :param password: password of device
@@ -16,15 +15,21 @@ class Network:
         self.keys = keys
 
     def command_cisco(self, hosts, set_command):
-        HOST = {
-                    'ip' : hosts,
-                    'username' : self.username,
-                    'password' : self.password,
-                    'use_keys' : self.keys,
-                    'device_type' : 'cisco_ios',
-            }
+        '''
+
+        :param hosts: hostname or IP of the device
+        :param set_command: command send to device, can be string or list
+        :return:
+        '''
+        device = {
+            'ip': hosts,
+            'username': self.username,
+            'password': self.password,
+            'use_keys': self.keys,
+            'device_type': 'cisco_ios',
+        }
         try:
-            net_connect = Netmiko(**HOST)
+            net_connect = Netmiko(**device, fast_cli=True)
             if isinstance(set_command, list):
                 result = []
                 for command in set_command:
@@ -35,24 +40,28 @@ class Network:
         except paramiko.ssh_exception.AuthenticationException:
             print("\nWrong Password\n")
 
-
     def command_firewall(self, hosts, set_command):
-        HOST = {
-                'ip' : hosts,
-                'username' : self.username,
-                'password' : self.password,
-                'use_keys' : self.keys,
-                'device_type' : 'fortinet',
+        device = {
+            'ip': hosts,
+            'username': self.username,
+            'password': self.password,
+            'use_keys': self.keys,
+            'device_type': 'fortinet',
         }
         try:
-            net_connect = Netmiko(**HOST)
-            return net_connect.send_command(set_command)
+            net_connect = Netmiko(**device, fast_cli=True)
+            if isinstance(set_command, list):
+                result = []
+                for command in set_command:
+                    result.append(net_connect.send_command(command))
+                return result
+            else:
+                return net_connect.send_command(set_command, auto_find_prompt=False)
         except paramiko.ssh_exception.AuthenticationException:
             print("\nWrong Password\n")
 
-
-    def command_slb(self,host, set_command):
-        HOST = {
+    def command_slb(self, host, set_command):
+        device = {
             'ip': host,
             'username': self.username,
             'password': self.password,
@@ -60,68 +69,69 @@ class Network:
             'device_type': 'piolink',
         }
         try:
-            net_connect = Netmiko(**HOST)
+            net_connect = Netmiko(**device)
             return net_connect.send_command(set_command)
         except paramiko.ssh_exception.AuthenticationException:
             print("\nWrong Password\n")
 
     def config_cisco(self, host, set_command):
-        HOST = {
-                'ip' : host,
-                'username' : self.username,
-                'password' : self.password,
-                'use_keys' : self.keys,
-                'device_type' : 'cisco_ios',
+        device = {
+            'ip': host,
+            'username': self.username,
+            'password': self.password,
+            'use_keys': self.keys,
+            'device_type': 'cisco_ios',
         }
         try:
-            net_connect = Netmiko(**HOST)
-            if isinstance(set_command, list):
-                result = []
-                for command in set_command:
-                    result.append(net_connect.send_config_set(command))
-                return result
-            else:
-                return net_connect.send_config_set(set_command)
+            net_connect = Netmiko(**device)
+            net_connect.send_config_set(set_command)
         except paramiko.ssh_exception.AuthenticationException:
             print("\nWrong Password\n")
 
     def config_file(self, host, set_file):
-        HOST = {
-                'ip' : host,
-                'username' : self.username,
-                'password' : self.password,
-                'use_keys' : self.keys,
-                'device_type' : 'cisco_ios',
+        '''
+        this method can be use if you want to write the command in file
+        :param host: IP or hostname the device
+        :param set_file: file location that contain some command
+        :return:
+        '''
+        device = {
+            'ip': host,
+            'username': self.username,
+            'password': self.password,
+            'use_keys': self.keys,
+            'device_type': 'cisco_ios',
         }
-        net_connect = Netmiko(**HOST)
+        net_connect = Netmiko(**device)
         return net_connect.send_config_from_file(set_file)
 
     def cisco_shell(self, host, set_command):
+        '''
+        This is shell prompt, it will be usefull for gather evidence to controlcase
+        :param host:
+        :param set_command:
+        :return:
+        '''
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(host, username=self.username, password=self.password)
         fshell = client.invoke_shell()
         fshell.send('terminal length 0\n')
-        fshell.recv(6500)
+        output = ''
         sleep(1)
+        fshell.recv(6500)
         if isinstance(set_command, list):
             for command in set_command:
                 fshell.send(command + '\n')
-                sleep(3)
+                sleep(2.5)
         else:
             fshell.send(set_command + '\n')
-            sleep(3)
-
+            sleep(1.5)
+        while True:
+            msg = fshell.recv(16)
+            if len(msg) <= 0:
+                break
+            output += msg.decode("utf-8")
         cmd_output = fshell.recv(65000)
         return cmd_output.decode(encoding='UTF-8')
         client.close()
-
-
-    def config_file(self, device, command):
-        net_connect = Netmiko(**device)
-        sleep(1)
-        net_connect.send_config_from_file(command)
-        sleep(1)
-        print( device['ip'] + ' Done')
-
-
